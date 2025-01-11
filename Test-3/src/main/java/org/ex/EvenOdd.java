222d95fdf303e980d95ce6073fa7d0964e1d10f4
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-01-11 16:35:57 +0530

    Even Odd

---
+++ Test-3/src/main/java/org/ex/EvenOdd.java
@@ -0,0 +1,14 @@
+package org.ex;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number <= 10) {
+            if (number % 2 == 0) {
+                System.out.print(number + " ");
+            }
+            number++;
+        }
+    }
+}
