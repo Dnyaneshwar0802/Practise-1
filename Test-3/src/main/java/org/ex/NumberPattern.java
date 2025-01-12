affb9093bf549061e6652618af30394e284c78ce
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-01-12 20:20:09 +0530

    Number Pattern

---
+++ Test-3/src/main/java/org/ex/NumberPattern.java
@@ -0,0 +1,22 @@
+package org.ex;
+
+import java.util.Scanner;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+
+        // Outer loop for rows
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
