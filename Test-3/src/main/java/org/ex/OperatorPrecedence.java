65567ff9d5334b33bc1de4b783a4a2e9e9261a5c
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-01-12 20:21:15 +0530

    OperatorPrecedence

---
+++ Test-3/src/main/java/org/ex/OperatorPrecedence.java
@@ -0,0 +1,11 @@
+package org.ex;
+
+public class OperatorPrecedence {
+        public static void main(String[] args) {
+            // Multiplication (*) and Division (/) have higher precedence than Addition (+) and Subtraction (-).
+            int result = 5 + 3 * 2 - 8 / 4;
+            System.out.println("Result of the expression 5 + 3 * 2 - 8 / 4 is: " + result);
+        }
+    }
+
+
